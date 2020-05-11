e46c8f15fa3e4ee21d5a032e36173cd65eff5fb4
CoderAariz <38472646+CoderAariz@users.noreply.github.com>
2020-05-11 15:25:06 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to FLIBO" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Georgia, "Times New Roman", Times, serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  font-size: 45px;
+  color: rgb(195, 241, 28);
+  margin-top: 0px;
+  background-image: url("./assets/back.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+</style>
